1b6b73530e69f1caf1b47705a416d3bcf683fced
BMIWALY <baimorn55@gmail.com>
2026-07-03 03:26:04 +0700

    feat: setup tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-function App(){
-
-return(
-
-<h1>
-
-Pocket Lingo Studio
-
-</h1>
-
-)
-
+function App() {
+    return (
+        <div className="min-h-screen bg-purple-100 flex items-center justify-center">
+            <h1 className="text-5xl font-bold text-purple-700">
+                Pocket Lingo Studio 🚀
+            </h1>
+        </div>
+    );
 }
 
-export default App
+export default App;
